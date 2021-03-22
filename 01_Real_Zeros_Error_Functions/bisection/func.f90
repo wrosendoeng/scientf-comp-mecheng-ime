f4014c6e488488a3a292a53c3413c5ebcf2ea2ae
wrosendoeng <wrosendoeng@gmail.com>
2021-03-22 18:27:34 -0300

    repairing some lines

--- 01_Real_Zeros_Error_Functions/bisection/func.f90
+++ 01_Real_Zeros_Error_Functions/bisection/func.f90
@@ -1,11 +1,10 @@
 MODULE func
-  implicit none
+  IMPLICIT NONE
   CONTAINS
     REAL(8) FUNCTION f(x)
       REAL(8),INTENT(IN) :: x
-
       !Create real polynomial function with known-zeros
       f = 4.0*cos(x) - exp(x)
       !f = x.**(3) - 9*x + 3
-    END function f
+  END function  f
 END MODULE func
