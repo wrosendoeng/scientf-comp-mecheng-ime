100eb4430d4aa20556f6f5b4b5db9cf63cb15c5e
wrosendoeng <wrosendoeng@gmail.com>
2021-03-23 15:55:51 -0300

    insertion of getopt.f90 module

--- 01_Real_Zeros_Error_Functions/bisection/main.f90
+++ 01_Real_Zeros_Error_Functions/bisection/main.f90
@@ -6,17 +6,33 @@ PROGRAM main
 
   !Deactivate implicit variables
   IMPLICIT NONE
-  REAL(8) :: a, b, x, bis
+  CHARACTER(len=32):: value_arg
+  REAL(8) :: a, b, x, bis, t
+  REAL(8), DIMENSION(2) :: vec
+  INTEGER :: i
 
-  !Assuming interval of a real function
-  OPEN(10, FILE="data.txt", ACCESS='DIRECT', ACTION='READ', &
-  STATUS='OLD',REC=exp)
+  !Assuming interval of a real function using a .txt file
+  !OPEN(10, FILE="data.txt",ACCESS='SEQUENTIAL', ACTION='READ', &
+  !STATUS='OLD',FORM='FORMATTED')
 
-  READ(10,'(F10.8)') a
-  READ(10,'(F10.8)') b
+  !READ(10,'(F10.8)') a
+  !READ(10,'(F10.8)') b
 
-  !a = 0
-  !b = 1.5
+  !Passing command-line arguments
+  i = 0
+  DO 
+    CALL GET_COMMAND_ARGUMENT(i,value_arg)
+    IF (LEN_TRIM(value_arg) == 0) EXIT
+	
+    WRITE(10,*) TRIM(value_arg)
+    READ(10,*) t
+    vec(i) = t	
+    i = i+1
+  END DO
+
+  a = vec(1)
+  b = vec(2)
+  
   x = (b+a)/2 ! arithmetic mean of interval [a,b]
   bis = bissec(f,a,b)
 
