38b926def431076ada1f4db2938a77df66b3f9c9
wrosendoeng <wrosendoeng@gmail.com>
2021-06-01 14:18:35 -0300

    "removing files"

--- final_project/helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-void print_square(double x){
-    cout << "the square of " << x << " is " << square(x) << "\n";
-    return 0;
-}
--- final_project/traj4dof.f90
+++
@@ -1,6 +0,0 @@
-program final_project
-    implicit none
-
-    
-
-end program final_project
